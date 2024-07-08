db86468b2e5251b45ad80012cd076f78a2567db0
Norbert Grover <norbertgrover@Norberts-MBP.attlocal.net>
2024-07-08 11:04:22 -0700

    First commit

---
+++ MovieCoordinatorWithUnitTest/MovieCoordinatorWithUnitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCoordinatorWithUnitTestApp.swift
+//  MovieCoordinatorWithUnitTest
+//
+//  Created by Norbert Grover on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieCoordinatorWithUnitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
