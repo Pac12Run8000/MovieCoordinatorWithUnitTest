08ccb2c4b9c85d5aabda60f5fc2ba4258cbd6242
Norbert Grover <norbertgrover@Norberts-MBP.attlocal.net>
2024-07-08 13:53:09 -0700

    everything is working

--- MovieCoordinatorWithUnitTest/Coordinators/AppCoordinator.swift
+++ MovieCoordinatorWithUnitTest/Coordinators/AppCoordinator.swift
@@ -3,26 +3,37 @@ import SwiftUI
 class AppCoordinator: ObservableObject, AppCoordinatorProtocol {
     @Published var currentView: AnyView = AnyView(ContentView())
     
+    // Properties to store current coordinators for testing
+    var trendingMoviesCoordinator: TrendingMoviesCoordinator?
+    var trendingShowsCoordinator: TrendingShowsCoordinator?
+    var movieSearchCoordinator: MovieSearchCoordinator?
+    var tvSearchCoordinator: TVSearchCoordinator?
+    
     func showTrendingMovies() {
         let coordinator = TrendingMoviesCoordinator()
+        trendingMoviesCoordinator = coordinator
         currentView = AnyView(TrendingMoviesView(coordinator: coordinator))
     }
     
     func showTrendingShows() {
         let coordinator = TrendingShowsCoordinator()
+        trendingShowsCoordinator = coordinator
         currentView = AnyView(TrendingShowsView(coordinator: coordinator))
     }
     
     func showMovieSearch() {
         let coordinator = MovieSearchCoordinator()
+        movieSearchCoordinator = coordinator
         currentView = AnyView(MovieSearchView(coordinator: coordinator))
     }
     
     func showTVSearch() {
         let coordinator = TVSearchCoordinator()
+        tvSearchCoordinator = coordinator
         currentView = AnyView(TVSearchView(coordinator: coordinator))
     }
 }
+
 
 class TrendingMoviesCoordinator: ObservableObject {}
 class TrendingShowsCoordinator: ObservableObject {}
--- MovieCoordinatorWithUnitTestTests/MovieCoordinatorWithUnitTestTests.swift
+++ MovieCoordinatorWithUnitTestTests/MovieCoordinatorWithUnitTestTests.swift
@@ -1,35 +1,41 @@
-//
-//  MovieCoordinatorWithUnitTestTests.swift
-//  MovieCoordinatorWithUnitTestTests
-//
-//  Created by Norbert Grover on 7/8/24.
-//
-
 import XCTest
+import SwiftUI
+@testable import MovieCoordinatorWithUnitTest
 
-final class MovieCoordinatorWithUnitTestTests: XCTestCase {
+final class AppCoordinatorTests: XCTestCase {
+    var appCoordinator: AppCoordinator!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        super.setUp()
+        appCoordinator = AppCoordinator()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        appCoordinator = nil
+        super.tearDown()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testShowTrendingMovies() {
+        appCoordinator.showTrendingMovies()
+        XCTAssertNotNil(appCoordinator.trendingMoviesCoordinator)
+        XCTAssertTrue(appCoordinator.currentView is AnyView)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testShowTrendingShows() {
+        appCoordinator.showTrendingShows()
+        XCTAssertNotNil(appCoordinator.trendingShowsCoordinator)
+        XCTAssertTrue(appCoordinator.currentView is AnyView)
     }
 
+    func testShowMovieSearch() {
+        appCoordinator.showMovieSearch()
+        XCTAssertNotNil(appCoordinator.movieSearchCoordinator)
+        XCTAssertTrue(appCoordinator.currentView is AnyView)
+    }
+
+    func testShowTVSearch() {
+        appCoordinator.showTVSearch()
+        XCTAssertNotNil(appCoordinator.tvSearchCoordinator)
+        XCTAssertTrue(appCoordinator.currentView is AnyView)
+    }
 }
